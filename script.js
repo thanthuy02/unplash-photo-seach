2f1cf20ff974c5039decafed1258256fb440e342
thanthuy02 <2051063434@e.tlu.edu.vn>
2022-06-09 18:15:43 +0700

    ..

--- script.js
+++ script.js
@@ -6,6 +6,9 @@
 //     let apiURL = `https://api.unsplash.com/photos/?client_id=${apiKey}&count=${initialCount}`;
 //     let response = await window.fetch(apiURL)
 //     let result = await response.json();
+//      B1: lay 1 chi so ngau nhien trong mang
+//      let index = Math.floor(Math.random()*result.length)
+//      console.log(result[index]);
 //     console.log(result);
 // }
 // getImg();
@@ -22,4 +25,24 @@ let apiURL = `https://api.unsplash.com/photos/?client_id=${apiKey}&count=${initi
 
 function updateCount(picCount){
     let apiURL = `https://api.unsplash.com/photos/?client_id=${apiKey}&count=${picCount}`;
+}
+
+//let ready = false;
+let totalImage = 0;
+let imagesLoaded = 0;
+function imageLoaded(){
+    imagesLoaded++;
+    console.log(imagesLoaded);
+    if(imagesLoaded === totalImage){
+        //ready = true;
+        loader.hidden = true;
+        //count = 20;
+    }
+}
+
+
+function setAttributes(elem, attr){
+    for(const keu in attr){
+        elem.setAttributes(key, attr[key]);
+    }
 }
